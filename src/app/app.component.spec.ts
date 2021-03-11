221c5d4625d513489ab7c4a4d1d494e51af604fa
Pintér József <ptrj1990@gmail.com>
2021-03-11 14:08:23 +0100

    chore: add angular-material and fontawesome to the project

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,4 +1,5 @@
 import { TestBed } from '@angular/core/testing';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
 import { HeaderModule } from './shared/components/header/header.module';
@@ -6,7 +7,7 @@ import { HeaderModule } from './shared/components/header/header.module';
 describe('AppComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [RouterTestingModule, HeaderModule],
+      imports: [RouterTestingModule, HeaderModule, BrowserAnimationsModule],
       declarations: [AppComponent]
     }).compileComponents();
   });
